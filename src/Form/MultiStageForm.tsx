243803e1a72c82c089b41428cbe96eedb03fcb9d
baljit <baljit@trint.com>
2020-08-16 21:39:12 +0100

    started breaking component out to a seperate form with pages

---
+++ src/Form/MultiStageForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function MultiStageForm() {
+  return(
+    <>
+    <div>User</div>
+    <div>Privacy</div>
+    <div>Done</div>
+    </>
+  )
+}
---
+++ src/Form/Pages/User.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+
+import { isEmailValid, isPasswordValid } from "../../Shared/utils";
+
+function validateEmail(email: string) {
+  if (email === "") {
+    return "Email must not be empty";
+  }
+
+  if (!isEmailValid(email)) {
+    return "Email must be valid";
+  }
+
+  return "";
+}
+
+function validatePassword(password: string) {
+  if (password === "") {
+    return "Password must not be empty";
+  }
+
+  if (password.length < 9) {
+    return "Password must be atleast 9 characters long";
+  }
+
+  if(!isPasswordValid(password)) {
+    return "Password must contain 1 uppercase letter, 1 lowercase letter and a number";
+  }
+
+  return "";
+}
+export function User() {
+  const [name, setName] = useState("");
+  const [nameError, setNameError] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  function validateAndSubmitForm() {
+    if (name === "") {
+      setNameError(true);
+    }
+    const emailValidation = validateEmail(email);
+    setEmailError(emailValidation);
+
+    const passwordValidation = validatePassword(password);
+    setPasswordError(passwordValidation);
+  }
+
+  return (
+    <>
+      <div>
+        <div>Name</div> {nameError && <span>Name must not be empty</span>}
+        <input
+          name="name"
+          aria-label="name-input"
+          placeholder="Name"
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </div>
+      <div>
+        <div>Role</div>
+        <input name="role" placeholder="Role" type="text" />
+      </div>
+      <div>
+        <div>Email</div> {emailError && <span>{emailError}</span>}
+        <input
+          name="email"
+          placeholder="Email"
+          type="text"
+          aria-label="email-input"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </div>
+      <div>
+        <div>Password</div>
+        {passwordError && <span>{passwordError}</span>}
+        <input
+          name="password"
+          placeholder="Password"
+          type="password"
+          aria-label="password-input"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </div>
+      <button onClick={validateAndSubmitForm}>Submit</button>
+    </>
+  );
+}
