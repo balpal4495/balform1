94e65c61188cd3dda023c30d19921207f474c90e
baljit <baljit@trint.com>
2020-08-17 15:07:08 +0100

    added done page

---
+++ src/Form/Pages/Done.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function Done() {
+  return (
+    <>
+      <div>
+        <div>Tick</div>
+        <div>
+          Please verify your email address, you should have recieved an email
+          from us already
+        </div>
+      </div>
+    </>
+  );
+}
